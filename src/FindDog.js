607853a2bab8080363a159baa36be552089f20fd
Robert Leu <robertleu@gmail.com>
2018-10-04 02:36:29 -0700

    Added ListDog Component

---
+++ src/FindDog.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class FindDog extends Component {
+    render() {
+        return (
+            <div class="control">
+                <div class="select">
+                    <select>
+                        <option value="default" disabled>
+                            Select Area
+                        </option>
+                        <option value="91740">San Dimas Area</option>
+                        <option value="92825">Anaheim Area</option>
+                        <option value="92501">Riverside Area</option>
+                    </select>
+                </div>
+            </div>
+        );
+    }
+}
+export default FindDog;
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import './css/bulma.css';
+import FindDog from './FindDog';
 
 class SideBar extends Component {
     render() {
         return (
             <Menu>
-                <h2 className="title is-4">Adopt A Pet</h2>
-                <input className="input is-normal" type="text" placeholder="Search by Zipcode" />
+                <h2 className="title is-4">Adopt A Dog</h2>
+                <FindDog />
             </Menu>
         );
     }
