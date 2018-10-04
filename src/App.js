d71cf25e1d7805ab926d733333623bd9fe13dcc2
Robert Leu <robertleu@gmail.com>
2018-10-04 01:55:53 -0700

    Organized files and added Bulma.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Container from './Container';
 import * as Petfinder from './PetFinder';
-import './App.css';
+import './css/App.css';
+import './css/bulma.css';
+import SideBar from './SideBar';
 
 class App extends Component {
     render() {
-        return <div className="App">{/*<Container />*/}</div>;
+        return (
+            <div className="App">
+                <SideBar />
+                {/*<Container />*/}
+            </div>
+        );
     }
 }
 
--- src/PetFinder.js
+++ src/PetFinder.js
@@ -1,28 +1,25 @@
 import fetchJsonp from 'fetch-jsonp';
 
 // Find Dogs
-export function getDogs(zip) {
-    let zipcode = zip;
+export const getDogs = zip =>
     // fetch parameters are specified  in URL query strings using '&' then argument name
     // as stated in petfinder API https://www.petfinder.com/developers/api-docs#methods
     fetchJsonp(
-        `http://api.petfinder.com/pet.find?format=json&key=0486cb8d84957db4db8abbb194319fdf&animal=dog&location=${zipcode}&output=full&callback=callback`,
+        `http://api.petfinder.com/pet.find?format=json&key=0486cb8d84957db4db8abbb194319fdf&animal=dog&location=${zip}&output=full&callback=callback`,
         { jsonpCallbackFunction: 'callback' }
     )
         .then(res => res.json())
-        .then(data => console.log(data.petfinder.pets.pet));
-}
+        .then(data => data.petfinder.pets.pet);
 
-export const getshelter = zip => {
-    let zipcode = zip;
+export const getShelter = zip =>
     // fetch parameters are specified  in URL query strings using '&' then argument name
     // as stated in petfinder API https://www.petfinder.com/developers/api-docs#methods
     fetchJsonp(
-        `http://api.petfinder.com/shelter.find?format=json&key=0486cb8d84957db4db8abbb194319fdf&location=${zipcode}&name=full&callback=callback`,
+        `http://api.petfinder.com/shelter.find?format=json&key=0486cb8d84957db4db8abbb194319fdf&location=${zip}&&count=5&callback=callback`,
         { jsonpCallbackFunction: 'callback' }
     )
         .then(res => res.json())
-        .then(data => console.log(data.petfinder.shelters.shelter))
+        .then(data => data.petfinder.shelters.shelter)
         .catch(err => console.log(err));
-};
-getshelter(91740);
+
+console.log(getDogs(91740));
