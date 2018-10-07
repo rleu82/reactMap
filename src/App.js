297118fe2af190952a8549b29a38c0fc36335e61
Robert Leu <robertleu@gmail.com>
2018-10-07 00:47:05 -0700

    Added API call for shelters and added map markers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import fetchJsonp from 'fetch-jsonp';
 import Container from './Container';
-import * as PetFinder from './PetFinder';
 import './css/App.css';
 import './css/bulma.css';
 import SideBar from './SideBar';
@@ -9,10 +8,11 @@ import SideBar from './SideBar';
 class App extends Component {
     state = {
         dogs: [],
-        searchZip: ''
+        shelters: [],
+        searchZip: '91740'
     };
     componentDidMount() {
-        this.findDogs(91740);
+        this.findShelters(91740);
     }
 
     findDogs = zip => {
@@ -23,17 +23,32 @@ class App extends Component {
             .then(res => res.json())
             .then(data => {
                 this.setState({ dogs: data.petfinder.pets.pet });
-            });
+            })
+            .catch(ex => console.log(ex));
     };
+
+    findShelters = zip => {
+        fetchJsonp(
+            `http://api.petfinder.com/shelter.find?format=json&key=0486cb8d84957db4db8abbb194319fdf&location=${zip}&callback=callback`,
+            { jsonpCallbackFunction: 'callback' }
+        )
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ shelters: data.petfinder.shelters.shelter });
+            })
+            .catch(ex => console.log(ex));
+    };
+
     updateZip = zip => {
         this.setState({ searchZip: zip });
     };
 
     render() {
+        console.log(this.state.shelters);
         return (
             <div className="App">
                 <SideBar dogs={this.state.dogs} updateZip={this.updateZip} />
-                {/*<Container />*/}
+                <Container shelters={this.state.shelters} />
             </div>
         );
     }
--- src/Container.js
+++ src/Container.js
@@ -2,13 +2,54 @@ import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class Container extends Component {
+    state = {
+        showInfoWindow: false,
+        clickedMarker: {},
+        selectedShelter: {}
+    };
+
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            selectedShelter: props,
+            clickedMarker: marker,
+            showInfoWindow: true
+        });
+
+    onMapClicked = props => {
+        if (this.state.showInfoWindow) {
+            this.setState({
+                showInfoWindow: false,
+                clickedMarker: null
+            });
+        }
+    };
+
     render() {
         return (
-            <Map google={this.props.google} zoom={14}>
-                <Marker onClick={this.onMarkerClick} name={'Current location'} />
-
-                <InfoWindow onClose={this.onInfoWindowClose}>
-                    <div />
+            <Map
+                onClick={this.onMapClicked}
+                google={this.props.google}
+                zoom={11}
+                initialCenter={{
+                    lat: 34.106676,
+                    lng: -117.806726
+                }}
+            >
+                {this.props.shelters.map(shelter => {
+                    return (
+                        <Marker
+                            onClick={this.onMarkerClick}
+                            name={shelter.name.$t}
+                            key={shelter.id.$t}
+                            title={shelter.name.$t}
+                            position={{ lat: shelter.latitude.$t, lng: shelter.longitude.$t }}
+                        />
+                    );
+                })}
+                <InfoWindow marker={this.state.clickedMarker} visible={this.state.showInfoWindow}>
+                    <div>
+                        <h1>{this.state.selectedShelter.name}</h1>
+                    </div>
                 </InfoWindow>
             </Map>
         );
